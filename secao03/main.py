2cb35217f5239f9b88753e1f3da3808a4bc2c0f2
Gabriel Correia <gabrielcorreia0913@gmail.com>
2023-06-26 21:29:18 -0300

    arquivos txt e main.py nao atualizados

--- secao03/main.py
+++ secao03/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+
 
 app = FastAPI()
 
@@ -24,8 +27,12 @@ async def get_cursos():
 
 @app.get('/cursos/{curso_id}')
 async def get_curso(curso_id: int):
-    curso = cursos[curso_id]
-    curso.update({"id": curso_id})
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado.")
+
     
     return curso
 
